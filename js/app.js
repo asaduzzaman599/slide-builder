d1ab89286fcde0156ab6e580ef4e58a411b2c6f0
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-10 14:55:36 +0600

    images displayed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const API_KEY = '15674931-a9d714b6e9d654524df198e00&q';
+const imagesContainer = document.querySelector('.images');
+const galleryContainer = document.querySelector('.gallery');
 
 const searchImages =async () =>{
 
@@ -9,8 +11,38 @@ const searchImages =async () =>{
     }
     const url = `https://pixabay.com/api/?key=${API_KEY}=${inputValue}&image_type=photo&pretty=true`;
 
+//    try{
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data);
+    displayImages(data);
+//    }catch(e){
+       /* alert('Sorry! Something is wrong.')
+       console.log(e) */
+//    }
+}
+
+const displayImages = ({hits}) =>{
+     console.log(hits);
+    imagesContainer.style.display = 'block';
+    
+    hits.forEach(image => {
+        const div = document.createElement('div');
+    
+        div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+    
+        div.innerHTML = `
+        <img class="img-fluid img-thumbnail"
+        onclick="selectItem('${image.webformatURL}')"
+        src="${image.webformatURL}" >
+        `;
+        
+        galleryContainer.appendChild(div);
+    
+    });
+    
+}
+
+const selectItem = (imageUrl) =>{
+    
 }
